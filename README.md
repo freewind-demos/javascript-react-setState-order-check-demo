78c972c953a15b8f89e042ea57cabdc6f1b5d125
freewind <nowindlee@gmail.com>
2018-09-24 18:14:20 +0800

    demo

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-JavaScript React Base Demo (Latest)
-===================================
+JavaScript React "setState" Order Check Demo
+============================================
 
-Always update with latest versions and technologies.
+React中的`setState()`是推迟执行的，这就意味着，如果它之后的代码使用`this.state`，得到的将会是更新前的数据。
+
+好在`setState`里还有一个参数，可以传一个function，将会在state改变之后执行，在那里可以拿到修改后的数据。
 
 ```
 npm install
 npm run dev
 ```
 
-Then visit <http://localhost:8080/>.
+![demo](./images/demo.jpg)
--- hello.jsx
+++ hello.jsx
@@ -1,11 +1,29 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react'
 
 class Hello extends Component {
-	render() {
-		return <div>
-		  <h1>Hello React</h1>
-		</div>;
-	}
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: 'react'
+        }
+    }
+
+    changeName() {
+        console.log('> changeName')
+        this.setState({
+            username: '(modified)'
+        }, function () {
+            console.log('username should changed to (modified): ', this.state.username)
+        })
+        console.log('username should not be changed at this time point: ', this.state.username)
+    }
+
+    render() {
+        return <div>
+            <h1>Hello {this.state.username}</h1>
+            <button type='button' onClick={this.changeName.bind(this)}>change name</button>
+        </div>
+    }
 }
 
-export default Hello;
+export default Hello
